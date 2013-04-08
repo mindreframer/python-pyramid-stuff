f248dd0ba6adab8d3ad68a316d6ce6f49f099dc5
Roman Heinrich <roman.heinrich@gmail.com>
2013-04-09 00:56:04 +0300

    Squashed 'pylons__pyramid_chameleon/' content from commit f8b4046
    git-subtree-dir: pylons__pyramid_chameleon
    git-subtree-split: f8b4046538e2f26142c3608f6f960656d49ee963

---
+++ docs/api.rst
@@ -0,0 +1,16 @@
+.. _pyramid_chameleon_api:
+
+:mod:`pyramid_chameleon` API
+----------------------------
+
+.. automodule:: pyramid_chameleon
+.. autofunction:: includeme
+
+
+.. automodule:: pyramid_chameleon.interfaces
+.. autointerface:: IChameleonLookup
+.. autointerface:: IChameleonTranslate
+
+.. automodule:: pyramid_chameleon.renderer
+
+
---
+++ pyramid_chameleon/__init__.py
@@ -0,0 +1,20 @@
+from translationstring import ChameleonTranslate
+from pyramid.config.i18n import translator
+
+from pyramid_chameleon import (
+    chameleon_text,
+    chameleon_zpt
+    )
+
+from pyramid_chameleon.interfaces import IChameleonTranslate
+
+
+def includeme(config):
+    """
+    Adds renderers for .pt and .txt as well as registers a Chameleon
+    translation utility
+    """
+    config.add_renderer('.pt', chameleon_zpt.renderer_factory)
+    config.add_renderer('.txt', chameleon_text.renderer_factory)
+    ctranslate = ChameleonTranslate(translator)
+    config.registry.registerUtility(ctranslate, IChameleonTranslate)
